99d7a511534006174f1f6e9cab428a8d72227ec3
Stephen Tapper <stephen@linux.(none)>
2012-12-08 11:43:52 -0500

    Created ant file for compiling code.

--- run.sh
+++ run.sh
@@ -9,33 +9,23 @@ minSlice="1000"
 maxSlice="1500"
 
 
-#create framework jar
-echo "----Creating framework jar"
-rm $frameworkDir/skynet.jar
-jar cf $frameworkDir/skynet.jar -C $frameworkDir/bin edu
-
-#create user code jar
-echo "----Creating user code jar"
-rm $userDir/afib-extractor.jar
-jar cf $userDir/afib-extractor.jar -C $userDir/bin edu
-
 #clear hdfs
 echo "----Clearing HDFS"
 hadoop dfs -rmr .
 
-#copy over data
+#copy over data to hdfs
 echo "----Copying files to HDFS"
 hadoop dfs -mkdir input
 hadoop dfs -copyFromLocal afib-data/*.* input/
 
-#run jar
+#run job
 echo "----Running job"
 hadoop jar $frameworkDir/skynet.jar edu.skynet.hadoop.MapRedDriver \
  -D minSlice=$minSlice -D maxSlice=$maxSlice -D exporter.class=$exporterClass \
  -libjars $userDir/afib-extractor.jar \
  input/ $extractorClass $sampleParserClass $annotationParserClass $sampleRate
 
-#copy results
+#copy results from HDFS to local machine
 echo "----Copying results to local machine"
 rm -r job-output/
 hadoop dfs -copyToLocal extractedData/ job-output/
